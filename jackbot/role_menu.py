c1e8022d86d60118c60b30039160f43e76410e32
John Hancock <john@jhnhnck.com>
2023-03-05 17:20:15 -0500

    Code cleanup

--- jackbot/role_menu.py
+++ jackbot/role_menu.py
@@ -1,5 +1,5 @@
 """
-JackBot - Controls roll menu
+JackBot - Controls role menu
 Author(s): @jhnhnck <john@jhnhnck.com>
 
 This file is licensed under the Apache License, Version 2.0; See LICENSE for full text.
