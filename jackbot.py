4979b2aa46cddff6635c1dd86b63dc81a0bca27c
John Hancock <john@jhnhnck.com>
2023-03-05 17:19:20 -0500

    Added dotenv config loading

--- jackbot.py
+++ jackbot.py
@@ -7,6 +7,10 @@ Author(s): @jhnhnck <john@jhnhnck.com>
 This file is licensed under the Apache License, Version 2.0; See LICENSE for full text.
 """
 
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
 from jackbot import core
 
 if __name__ == '__main__':
