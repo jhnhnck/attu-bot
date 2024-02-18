4a750b2e9c1d30a7a5111726152be01dddd0ae7d
John Hancock <jhnhnck@proton.me>
2024-02-18 12:22:39 -0500

    Bot changes

--- attubot/core.py
+++ attubot/core.py
@@ -6,15 +6,18 @@ This file is licensed under the Apache License, Version 2.0; See LICENSE for ful
 """
 
 from os import getenv
+from datetime import datetime, date, time
+from pytz import timezone
+import re
 
 import discord
-from discord.utils import escape_mentions
+from discord.ext import tasks
 
-from jackbot.config import Config
-from jackbot.logging import get_logger
+from attubot.config import Config
+from attubot.logging import get_logger
 logger = get_logger(__name__)
 
-from jackbot import role_menu
+from attubot.wiki import AttuWiki
 
 # --- Initialization ---
 
@@ -22,76 +25,145 @@ logger.info('Initializing...')
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.reactions = True
 
 bot = discord.Bot(intents=intents)
 config = Config(getenv('BOT_CONFIG_FILE'))
 
+separators = ["<","=","+","\>","/","&",":","$","\*","%","@", "⁂", "xXx","\\\\","?","^","\|","\~", '-']
+flipped_separators = { '<': '>', '\>': '<', '/': '\\\\', '\\\\': '/' }
+
+build_format = '%a %b %d %H:%M:%S %Z %Y'
+
+# --- Utilities ---
+
+def format_year_line(year):
+    global separators, escaped_separators, flipped_separators
+
+    sep = separators[year % len(separators)]
+
+    if len(sep) > 2:
+        return f'# {sep} Year {year} PC {sep}'
+    if sep in flipped_separators:
+        return f'# {sep * 3} Year {year} PC {flipped_separators[sep] * 3}'
+    else:
+        return f'# {sep * 3} Year {year} PC {sep * 3}'
+
 # --- Slash Commands ---
 
-role_menu.init(bot)
+@bot.slash_command(guilds_only=True)
+async def check_year(ctx):
+    global bot, config
+    guild = bot.get_guild(config.guild)
+
+    # --- Checks ---
+
+    days_since_epoch =  (date.today() - date.fromtimestamp(config.epoch_time)).days
+    year = config.epoch_year + (days_since_epoch // 14)
+
+    if days_since_epoch % 14 != 0:
+        await ctx.respond(f'{14 - (days_since_epoch % 14)} Days Remaining Until Year {year + 1} PC')
+    else:
+        await ctx.respond(f'Happy New Year! Advancing to Year {year} PC at <t:1708207200:t>')
 
 @bot.slash_command(guilds_only=True)
-async def gametime(ctx, message: str=None):
-    if message != None and len(message) > 0:
-        await ctx.respond(f"<@&1082043670620024924> from {ctx.author.mention}: `{escape_mentions(message)}`")
-    else:
-        await ctx.respond(f"<@&1082043670620024924> from {ctx.author.mention}!")
+@discord.commands.option(name='channel', required=True, description='Lore Channel', input_type=discord.TextChannel)
+@discord.commands.option(name='year', required=True, description='Year Number', input_type=int)
+async def link_year(ctx, channel: discord.TextChannel, year: int):
+    global bot, config
 
-# --- Reactions ---
+    if channel.id not in config.lore_channels and channel.id != config.meta_chat_channel:
+        await ctx.respond('Failed: Channel is not a lore channel.')
+        return
 
-@bot.event
-async def on_raw_reaction_add(payload):
-    """
-    <RawReactionActionEvent
-        message_id=1082050030925512765
-        user_id=263116922793623555
-        channel_id=1082050009010282546
-        guild_id=572148465870700544
-        emoji=<PartialEmoji animated=False name='jh_green' id=619258364094054440>
-        event_type='REACTION_ADD'
-        member=<Member id=263116922793623555 name='jhnhnckjr' discriminator='6220' bot=False nick=None
-            guild=<Guild id=572148465870700544 name='PIVOT Playground' shard_id=0 chunked=False member_count=5>
-        >>
-    """
-    user = payload.user_id
-    reaction = payload.emoji
-    message_id = payload.message_id
-    logger.info(f'Added {reaction} on {message_id}: {user}')
+    if year < 1 or year > len(config.timestamps):
+        await ctx.respond(f'Failed: Pick a year between 1 and {len(config.timestamps)}.')
+        return
 
-@bot.event
-async def on_raw_reaction_remove(payload):
-    """
-    <RawReactionActionEvent
-        message_id=1082050030925512765
-        user_id=263116922793623555
-        channel_id=1082050009010282546
-        guild_id=572148465870700544
-        emoji=<PartialEmoji animated=False name='jh_green' id=619258364094054440>
-        event_type='REACTION_ADD'
-        member=<Member id=263116922793623555 name='jhnhnckjr' discriminator='6220' bot=False nick=None
-            guild=<Guild id=572148465870700544 name='PIVOT Playground' shard_id=0 chunked=False member_count=5>
-        >>
-    """
-    user = payload.user_id
-    reaction = payload.emoji
-    message_id = payload.message_id
-    logger.info(f'Removed {reaction} on {message_id}: {user}')
+    # Send message link
+    await ctx.respond(f'https://discord.com/channels/{config.guild}/{channel.id}/{config.timestamps[year - 1]}')
+
+@bot.slash_command(guilds_only=True)
+async def build_date(ctx):
+    build_time = datetime.strptime(getenv("BUILD_TIME"), build_format)
+    await ctx.respond(f'Container Build Time: <t:{int(build_time.timestamp())}:f>')
+
+# --- Tasks ---
+
+@tasks.loop(time=time(17, 00, tzinfo=timezone('EST')))
+async def check_for_new_year():
+    global bot, config
+    guild = bot.get_guild(config.guild)
+
+    # --- Checks ---
+
+    days_since_epoch =  (date.today() - date.fromtimestamp(config.epoch_time)).days
+    year = config.epoch_year + (days_since_epoch // 14)
+
+    if days_since_epoch % 14 != 0:
+        logger.info(f'Days Remaining Until Year {year + 1} PC: {14 - (days_since_epoch % 14)}')
+        return
+
+    logger.info(f'Happy New Year! Advancing to Year {year} PC')
+
+    # --- Lore Channel Year Markers ---
+
+    year_str = format_year_line(year)
+    message_links = []
+
+    for channel_id in config.lore_channels:
+        channel = guild.get_channel(channel_id)
+        message = await channel.send(year_str)
+        message_links.append(message.jump_url)
+
+    # Save timestamp to config file
+    config.add_timestamp(message.id)
+
+    # --- Increase Year VC ---
+
+    year_vc = guild.get_channel(config.year_vc)
+    await year_vc.edit(name=f'Current Year: {year} PC')
+
+    # --- Edit Wiki ---
+
+    wiki = AttuWiki()
+    wiki.authenticate(config.wiki_user, config.wiki_key)
+
+    text = wiki.get_page_contents(config.wiki_page)
+    updated_page = re.sub(r'Current Year: [\d]+ PC', f'Current Year: {year} PC', text, flags=re.IGNORECASE)
+
+    wiki.edit(config.wiki_page, updated_page, f'Bumped to Year {year} PC')
+
+    # --- Make Annoucement ---
+    channel = guild.get_channel(config.announce_channel)
+    await channel.send(f'<@&{config.announce_role}> Year {year} PC. (weap)')
+
+    # --- Send Year Links Message
+
+    thread = guild.get_channel(config.year_link_thread)
+    await thread.send(year_str + '\n' + '\n'.join(message_links))
 
 # --- Events ---
 
 @bot.event
 async def on_ready():
-    perms = '1512634297568'
+    perms = '207952'
 
     logger.info(f'Logged in as {bot.user} (ID: {bot.user.id})!')
     logger.info(f'Add to a server:\n\thttps://discordapp.com/oauth2/authorize?client_id={bot.application_id}&scope=bot&permissions={perms}')
 
-    # await load_role_menu()
+    check_for_new_year.start()
 
 @bot.event
 async def on_message(message):
+    if message.channel.id == config.activity_channel and message.content.startswith('[DoomBot]'):
+        await message.add_reaction('💖')
+
+
+"""
+@bot.event
+async def on_message(message):
     logger.info(f'Message from {message.author}: {message.content}')
+"""
 
 # --- Trigger Function ---
 
--- attubot/wiki.py
+++ attubot/wiki.py
@@ -4,3 +4,47 @@ Author(s): @jhnhnck <john@jhnhnck.com>
 
 This file is licensed under the Apache License, Version 2.0; See LICENSE for full text.
 """
+
+import requests
+
+class AttuWiki:
+    api_endpoint = 'https://wiki.attuproject.org/api.php'
+    session = requests.Session()
+    token = ''
+
+    def authenticate(self, user, key):
+        res = self.session.get(self.api_endpoint, params={ 'action':"query", 'meta': 'tokens', 'type': 'login', 'format': 'json' })
+        self.token = res.json()['query']['tokens']['logintoken']
+
+        data = {
+            'action': 'login',
+            'lgname': user,
+            'lgpassword': key,
+            'lgtoken': self.token,
+            'format': 'json'
+        }
+
+        res = self.session.post(self.api_endpoint, data=data)
+
+    def get_page_contents(self, page_name):
+        res = self.session.get(self.api_endpoint, params={ 'action': 'parse', 'page': page_name, 'prop': 'wikitext', 'formatversion': 2 , 'format': 'json' })
+
+        return res.json()['parse']['wikitext']
+
+    def edit(self, page_name, text, reason):
+        res = self.session.get(self.api_endpoint, params={ 'action': 'query', 'meta': 'tokens', 'format': 'json' })
+        csrf = res.json()['query']['tokens']['csrftoken']
+
+        data = {
+            'action': 'edit',
+            'title': page_name,
+            'token': csrf,
+            'format': 'json',
+            'text': text,
+            'bot': True,
+            'minor': True,
+            'summary': reason,
+        }
+
+        res = self.session.post(self.api_endpoint, data=data)
+        print(res.text)
