f5dd05f1287c4905b006ba8395a4d351483d1a4a
John Hancock <jhnhnck@proton.me>
2025-03-10 22:40:50 -0400

    Fix error log tracebacks

--- attubot/core.py
+++ attubot/core.py
@@ -30,7 +30,8 @@ async def send_to_error_log(error):
     guild = bot.get_guild(Config.jhn_guild)
     error_log = guild.get_channel(Config.error_log_channel)
 
-    tb_str = ''.join(traceback.format_tb(error.__traceback__))
+    # TODO: this still prints the useless bits
+    tb_str = ''.join(traceback.format_exception(error))
 
     await error_log.send(f'**{error}**\n```\n{tb_str}```')
 
