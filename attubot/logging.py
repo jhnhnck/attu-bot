95abb17a662b4fb85322627418b08e9298834a1f
John Hancock <jhnhnck@proton.me>
2025-03-14 23:59:45 -0400

    Cleanup

--- attubot/logging.py
+++ attubot/logging.py
@@ -3,7 +3,7 @@ AttuBot - Logging wrapper
 Author(s): @jhnhnck <john@jhnhnck.com>
 
 This file is licensed under the Apache License, Version 2.0; See LICENSE for full text.
-"""  # noqa: A005
+"""
 
 import sys
 from os import environ
