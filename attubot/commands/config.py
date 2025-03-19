664d84eafc569c7b77300ff8abe377bb03629fb6
John Hancock <jhnhnck@proton.me>
2025-03-18 21:10:20 -0400

    Redo stub functions

--- attubot/commands/config.py
+++ attubot/commands/config.py
@@ -7,11 +7,9 @@ This file is licensed under the Apache License, Version 2.0; See LICENSE for ful
 
 import discord
 from discord import Permissions
-from discord.ext import commands
 
 from attubot.config import NovaConfig
 from attubot.logging import get_logger
-from attubot.util import is_bot_owner
 
 logger = get_logger(__name__)
 
@@ -19,20 +17,33 @@ logger = get_logger(__name__)
 
 config_group = discord.SlashCommandGroup('config', default_member_permissions=Permissions.all(), description='Modify various options for bot behavior (Admin only)')
 
+@config_group.command(name='delete', description='Delete a specific key from the config table')
+@discord.commands.option(name='key', required=True, description='Config identifier', input_type=str)
+async def config_delete(ctx, key: str):
+    await ctx.respond('Oops. Not Implemented!', ethemeral=True)
+
+
 @config_group.command(name='get', description='Fetch the value of a configuration')
 @discord.commands.option(name='key', required=True, description='Config identifier', input_type=str)
 async def config_get(ctx, key: str):
-    await ctx.respond('Oops. Not Implemented!')
+    await ctx.respond('Oops. Not Implemented!', ethemeral=True)
+
+
+@config_group.command(name='list', description='List out the available config options')
+async def config_list(ctx):
+    await ctx.respond('Available Options:\n' + ''.join([f'- {x}\n' for x in NovaConfig.guild_keys]))
+
 
 @config_group.command(name='set', description='Set the value of a configuration')
 @discord.commands.option(name='key', required=True, description='Config identifier', input_type=str)
 @discord.commands.option(name='value', required=True, description='', input_type=str)
 async def config_set(ctx, key: str, value: str):
-    await ctx.respond('Oops. Not Implemented!')
+    await ctx.respond('Oops. Not Implemented!', ephemeral=True)
+
 
 @config_group.command(name='show', description='Show the entire server configuration')
 async def config_show(ctx):
-    await ctx.respond('Oops. Not Implemented!')
+    await ctx.respond('Oops. Not Implemented!', ethemeral=True)
 
 # --- Extension Def ---
 
@@ -40,4 +51,3 @@ def setup(bot):
     logger.info(f'Registered: {__name__}')
 
     bot.add_application_command(config_group)
-
