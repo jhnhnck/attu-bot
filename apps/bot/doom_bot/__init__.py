e26f066b08982b7d86e1e8f6e4b6726c501be19a
John Hancock <jhn@xffxe4.lol>
2026-05-07 02:25:34 +0000

    chore(deploy): bump version to 79.0

--- apps/bot/doom_bot/__init__.py
+++ apps/bot/doom_bot/__init__.py
@@ -12,7 +12,7 @@ __config_version__ = '2.5.4'  # minimum compatible config file version; bump onl
 __email__ = 'doom@attuproject.org'
 __description__ = 'A discord bot designed for automating tasks for the Attu Project'
 
-__version__ = '78.1.6'
+__version__ = '79.0'
 __build_time__ = 'Thu Aug 11 02:23:20 UTC 2022'  # stamped during docker build
 
 # --- Re-exports for backward compatibility ---
